ce9e3da42490444aea402b25c7792422fc9886c2
Uday <jnvudayju@gmail.com>
2023-09-28 18:02:48 +0530

    Method to solve Problem4

---
+++ carsProject/Problem4.js
@@ -0,0 +1,19 @@
+// ==== Problem #4 ====
+// The accounting team needs all the years from every car on the lot.
+// Execute a function that will return an array from the dealer data
+// containing only the car years and log the result in the console as it was returned.
+let result = [];
+function carYearData(data) {
+  for (let index = 0; index < data.length; index++) {
+    let d = data[index].car_year;
+    result.push(d);
+  }
+
+  return result;
+  // for custom sorting
+  //   return result.sort((a, b) => {
+  //     return b - a;
+  //   });
+}
+
+module.exports = carYearData;
