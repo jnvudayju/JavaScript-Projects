14315b8101b09511a180704df7f5d7ec04720747
Uday <jnvudayju@gmail.com>
2023-09-28 15:04:47 +0530

    call the function to solve problem2

---
+++ carsProject/tests/testProblem2.js
@@ -0,0 +1,10 @@
+// ==== Problem #2 ====
+// // The dealer needs the information on the last car in their inventory.
+// Execute a function to find what the maker and model of the last car in the inventory is?
+//  Log the maker and model into the console in the format of:
+// ("Last car is a *car make goes here* *car model goes here*");
+
+const data = require("../message");
+const lastCarData = require("../Problem2");
+const result = lastCarData(data);
+console.log(result);
