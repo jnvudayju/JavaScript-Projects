5949fe3c9d50577c8f40078f9a9eed59799431f1
Uday <jnvudayju@gmail.com>
2023-09-28 18:28:53 +0530

    Modification to call function in differnt way to solve Problem4

--- carsProject/Problem4.js
+++ carsProject/Problem4.js
@@ -2,8 +2,9 @@
 // The accounting team needs all the years from every car on the lot.
 // Execute a function that will return an array from the dealer data
 // containing only the car years and log the result in the console as it was returned.
-let result = [];
+
 function carYearData(data) {
+  let result = [];
   for (let index = 0; index < data.length; index++) {
     let d = data[index].car_year;
     result.push(d);
