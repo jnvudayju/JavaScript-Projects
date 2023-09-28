b0f41ae1a9b2826c877d8430cee74878787421ff
Uday <jnvudayju@gmail.com>
2023-09-28 15:02:34 +0530

    commented some lines

--- carsProject/message.js
+++ carsProject/message.js
@@ -102,11 +102,11 @@ let inventory = [
 
 // ==== Problem #1 ====
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by calling a function that will return the data for that car. Then log the car's year, make, and model in the console log in the format of:
-("Car 33 is a *car year goes here* *car make goes here* *car model goes here*");
+// ("Car 33 is a *car year goes here* *car make goes here* *car model goes here*");
 
 // ==== Problem #2 ====
-// The dealer needs the information on the last car in their inventory. Execute a function to find what the make and model of the last car in the inventory is?  Log the make and model into the console in the format of:
-("Last car is a *car make goes here* *car model goes here*");
+// // The dealer needs the information on the last car in their inventory. Execute a function to find what the make and model of the last car in the inventory is?  Log the make and model into the console in the format of:
+// ("Last car is a *car make goes here* *car model goes here*");
 
 // ==== Problem #3 ====
 // The marketing team wants the car models listed alphabetically on the website. Execute a function to Sort all the car model names into alphabetical order and log the results in the console as it was returned.
