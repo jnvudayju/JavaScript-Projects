bf28664ef13da9f8640a908e348ac161c685e311
Uday <jnvudayju@gmail.com>
2023-10-05 15:45:05 +0530

    Invoked method to solve problem2

---
+++ stringProject/problem2.js
@@ -0,0 +1,29 @@
+// Given an IP address - "111.139.161.143". Split it into its component parts 111, 139, 161, 143 and return it in an array in numeric values. [111, 139, 161, 143].
+
+// Support IPV4 addresses only. If there are other characters detected, return an empty array.
+
+function splitIP(data) {
+  if (data.length <= 0) {
+    return [];
+  }
+
+  for (let i = 0; i < data.length; i++) {
+    if (
+      !(data.charAt(i) >= "0" && data.charAt(i) <= "9") &&
+      data.charAt(i) != "."
+    ) {
+      return [];
+    }
+  }
+
+  let result = [];
+
+  for (let i = 0; i < data.length; i += 4) {
+    let temp = data.slice(i, i + 3);
+    result.push(temp);
+  }
+
+  return result;
+}
+
+module.exports = splitIP;
