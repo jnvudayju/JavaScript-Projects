07be608709ec56a4e88bc82ff656b9e31bf233ea
Uday <jnvudayju@gmail.com>
2023-10-12 17:29:34 +0530

    Built method counterFactory

---
+++ closureProject/counterFactory.js
@@ -0,0 +1,20 @@
+// Return an object that has two methods called `increment` and `decrement`.
+// `increment` should increment a counter variable in closure scope and return it.
+// `decrement` should decrement the counter variable and return it.
+
+function counterFactory() {
+  let counter = 0;
+  return {
+    increment: () => {
+      counter = counter + 1;
+      return counter;
+    },
+
+    decrement: () => {
+      counter = counter - 1;
+      return counter;
+    },
+  };
+}
+
+module.exports = counterFactory;
